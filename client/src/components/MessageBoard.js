d421973de481af9f38df3b0f61b089815eaff7f4
Richie Bonilla <richie@richie.co>
2019-09-07 18:08:23 -0400

    Update Troll Box styling

--- client/src/components/MessageBoard.js
+++ client/src/components/MessageBoard.js
@@ -12,10 +12,10 @@ class MessageBoard extends Component {
 
   componentDidMount() {
     this.updateMessageBoard();
-    const accounts = ethereum.enable();
-    const account = account[0];
+    // const accounts = ethereum.enable();
+    // const account = account[0];
 
-    this.setState({ account });
+    // this.setState({ account });
   }
 
   handleChange = e => {
@@ -23,7 +23,9 @@ class MessageBoard extends Component {
   };
 
   handleSubmit = e => {
+    e.preventDefault();
     const { message } = this.state;
+    if (message.length < 1) return;
     axios
       .post("/api/newMessage/", { message })
       .then(res => {
@@ -31,12 +33,11 @@ class MessageBoard extends Component {
         this.setState({ message: "", messages: res.data.reverse() });
       })
       .catch(err => console.log(new Error(err)));
-    e.preventDefault();
   };
 
   updateMessageBoard = () => {
     axios.get("/api/getMessages/").then(res => {
-      this.setState({ messages: res.data });
+      this.setState({ messages: res.data.reverse() });
     });
   };
 
@@ -44,18 +45,25 @@ class MessageBoard extends Component {
     return (
       <div className="message-board body">
         <div>
-          <h3 style={{ marginBottom: "12px", marginLeft: "12px" }}>
-            Message Board
+          <h3
+            style={{
+              textAlign: "center",
+              marginBottom: "12px",
+              marginLeft: "12px"
+            }}
+          >
+            Troll Box
           </h3>
         </div>
         <div className="comment-box">
           <form type="POST" onSubmit={this.handleSubmit}>
             <input
+              type="text"
               onChange={this.handleChange}
               value={this.state.message}
               placeholder="Add your message..."
             />
-            <input type="submit" value="POST" />
+            <button type="submit">POST</button>
           </form>
         </div>
         <Messages messages={this.state.messages} />
