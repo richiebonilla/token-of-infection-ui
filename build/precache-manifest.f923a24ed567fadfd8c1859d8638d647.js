ceb19449f0f8e01381fca23498675f1e126bc8aa
Richie Bonilla <richie@richie.co>
2019-09-07 02:00:29 -0400

    Update gitignore

---
+++ build/precache-manifest.f923a24ed567fadfd8c1859d8638d647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8051784520ec1ccd6cf28d4209cf41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7339f3da091cbda9611f",
+    "url": "/static/css/main.1914a169.chunk.css"
+  },
+  {
+    "revision": "1e0dc8f11c60e5f98a5f",
+    "url": "/static/js/2.f9dec275.chunk.js"
+  },
+  {
+    "revision": "7339f3da091cbda9611f",
+    "url": "/static/js/main.cc1c42ba.chunk.js"
+  },
+  {
+    "revision": "f5c95e3627a0d72a0be3",
+    "url": "/static/js/runtime~main.1cc18454.js"
+  }
+]);
