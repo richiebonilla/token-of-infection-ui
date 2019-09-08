a2b8b81b5ac7764816431be143cf410541514ff1
Cybourgeoisie <rbenheidorn@gmail.com>
2019-09-08 10:07:38 -0400

    new buidl

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fae9dd3b1e20de98e2d94b9e315a1661.js"
+  "/precache-manifest.41c7c9a097d7f033de46de9d3e2ff624.js"
 );
 
 self.addEventListener('message', (event) => {
