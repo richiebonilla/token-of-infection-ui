4f9c75d6fb8f7816df913e7dfbd523b4847a49d5
Richie Bonilla <richie@richie.co>
2019-09-08 12:22:36 -0400

    build

---
+++ client/build/precache-manifest.9b1c71eaaa7d04c9aa78b2225f855857.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11639954723b77b228e7b04c53332ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "570a9cff4b1586bdf9ad",
+    "url": "/static/css/main.a97c96fb.chunk.css"
+  },
+  {
+    "revision": "b7a82ab50fefafc2693e",
+    "url": "/static/js/2.133faba9.chunk.js"
+  },
+  {
+    "revision": "570a9cff4b1586bdf9ad",
+    "url": "/static/js/main.b8e3cb57.chunk.js"
+  },
+  {
+    "revision": "f5c95e3627a0d72a0be3",
+    "url": "/static/js/runtime~main.1cc18454.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41c7c9a097d7f033de46de9d3e2ff624.js"
+  "/precache-manifest.9b1c71eaaa7d04c9aa78b2225f855857.js"
 );
 
 self.addEventListener('message', (event) => {
